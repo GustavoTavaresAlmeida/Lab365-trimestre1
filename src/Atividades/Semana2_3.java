443a2d0e7e0ab527fba323c96ad7fa7a829ac4d1
GustavoTavares <gustavotavares0212@gmail.com>
2025-02-19 21:16:20 -0300

    atividade 3 configurações do PI correta 2

--- src/Atividades/Semana2_3.java
+++ src/Atividades/Semana2_3.java
@@ -5,12 +5,12 @@ public class Semana2_3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite o raio da circunferencia: ");
+        System.out.println("Digite o raio da circulo: ");
         double raio = scanner.nextDouble();
 
-        double area = 3.14159 * (raio +raio);
+        double area = 3.14159 * (raio * raio);
 
-        System.out.println("A área da circunferência é: " + area);
+        System.out.println("A área da circulo é: " + area);
 
         scanner.close();
     }
